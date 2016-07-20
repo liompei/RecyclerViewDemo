b71052fb37bc034be73b4efa4392a92c0c2c17d7
liompei <blm1137694912@gmail.com>
2016-07-20 22:29:41 +0800

    Initial commit

---
+++ app/src/main/java/com/liompei/recyclerviewdemo/MainActivity.java
@@ -0,0 +1,47 @@
+package com.liompei.recyclerviewdemo;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.StaggeredGridLayoutManager;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * RecyclerView(开始使用)
+ */
+public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView recyclerView;
+    private RvAdapter adapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        initView();
+    }
+
+    private void initView() {
+        recyclerView = (RecyclerView) findViewById(R.id.recyclerView);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));  //必备
+        adapter = new RvAdapter(this, getData());
+        recyclerView.setAdapter(adapter);  //必备
+    }
+
+    private List<Map<String, String>> getData() {
+        List<Map<String, String>> mapList = new ArrayList<>();
+        for (int i = 0; i < 100; i++) {
+            Map<String, String> map = new HashMap<>();
+            map.put("name", "liompei" + i);
+            map.put("num", "" + i);
+            mapList.add(map);
+        }
+        return mapList;
+    }
+
+}
